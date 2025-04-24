6128f7083f143638c93da8a25aa8ff214333241e
Lukas <lukas.kubala@gmx.de>
2025-04-24 13:29:44 +0200

    Initialer Commit

---
+++ mein-projekt/src/test/java/com/example/mein_projekt/MeinProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mein_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeinProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
